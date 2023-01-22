9b6c03ca897ef435a2b7d6be676d381ab071f0e0
donnwilzy <ask4godswill@gmail.com>
2023-01-22 23:55:22 +0100

    fetching weather api

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,5 +1,7 @@
  import axios from 'axios';
 const URL = 'https://api.openweathermap.org/data/2.5/weather'
+const API_KEY = '2222233YGGHGGJGJGJGJGJGJG'
+
  const fetchWeather = async (query)=>{
-    const resposnse = await axios.get();
+    const resposnse = await axios.get(URL);
  }
