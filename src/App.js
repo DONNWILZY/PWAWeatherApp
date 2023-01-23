e43bf28140f805fc45ccc48f19d796e722718f99
donnwilzy <ask4godswill@gmail.com>
2023-01-23 05:03:35 +0100

    fetching weather api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React , {useState} from 'react';
+import './App.css';
+
 
 const App = ()=>{
+    const [query, setQuery] = useState('');
+    const search = async (e) =>{
+        if(e.key === 'enter'){
+            const data = await fetchWeather(query)
+        }
+    }
     return (
-        <h1>App</h1>
+        <div className = 'main-container'>
+            <input
+                type= 'text'
+                className='search'
+                placeholder='Search'
+                value = {query}
+                onChange ={(e)=> setQuery(e.target.value)}
+            /> 
+        </div> 
     );
 }
 
--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,7 +1,10 @@
  import axios from 'axios';
-const URL = 'https://api.openweathermap.org/data/2.5/weather'
-const API_KEY = '2222233YGGHGGJGJGJGJGJGJG'
+const URL = 'https://api.openweathermap.org/data/2.5/weather';
+const API_KEY = '7f9fba3c186237d8fc1e66c6ecac8d49' ;
+I
 
  const fetchWeather = async (query)=>{
-    const resposnse = await axios.get(URL);
+    const resposnse = await axios.get(URL, {
+
+    });
  }
