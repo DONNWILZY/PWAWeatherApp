cb986d3089bc6d309e9f0b6a956480a62201a4b2
donnwilzy <ask4godswill@gmail.com>
2023-01-24 23:56:26 +0100

    configuring weathe api

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,6 +1,8 @@
  import axios from 'axios';
-const URL = 'https://api.openweathermap.org/data/2.5/weather';
-const API_KEY = '7f9fba3c186237d8fc1e66c6ecac8d49' ;
+ const URL = 'https://api.openweathermap.org/data/2.5/weather';
+const URLi = 'https://api.openweathermap.org/data/2.5/weather';
+const API_KEY = 'f33a484cf794d08d0148764789aaba32';
+const API_KEY_ = '7f9fba3c186237d8fc1e66c6ecac8d49';
 I
 
  export const fetchWeather = async (query)=>{
