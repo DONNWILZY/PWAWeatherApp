a20143ff47cd478ffea0721ce907d4271e075147
donnwilzy <ask4godswill@gmail.com>
2023-01-25 22:06:09 +0100

    configuring weathe api

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import  ReactDOM  from 'react-dom';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 
- import App from './App';
+import App from './App'
 
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
 
- ReactDOM.render(<App />, document.getElementById('root')); 
+root.render(<StrictMode>, <App />,  </StrictMode>,);
